1ce78af74e689ccd90559ea43a2e98a2518e05ad
JuFeng Zhang <zjffun@gmail.com>
2021-08-31 23:26:16 +0800

    save

---
+++ packages/code-snippets-editor/src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CodeSnippetsEditor from "./CodeSnippetsEditor";
+
+ReactDOM.render(<CodeSnippetsEditor />, document.getElementById("root"));
